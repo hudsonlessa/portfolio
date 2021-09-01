0745d9820067af4a74a53a81544cc2196a58a071
Hudson Lessa <admin@hudsonlessa.com>
2021-09-01 13:18:48 -0300

    Add basic style reset

--- styles/global.js
+++ styles/global.js
@@ -1,3 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
-export default createGlobalStyle``;
+export default createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  border: 0;
+  outline: 0;
+}`;

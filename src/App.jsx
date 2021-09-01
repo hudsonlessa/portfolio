f76f2955db966cef344c6427eb6f8231b4556281
Hudson Lessa <admin@hudsonlessa.com>
2021-09-01 12:59:54 -0300

    Add basic theming support

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from '../styles/global';
+import { lightTheme, darkTheme } from '../themes';
 
 export default function App() {
-	return <></>;
+	const [currentTheme, setCurrentTheme] = useState(null);
+
+	useEffect(() => {
+		const today = new Date();
+		const currentHour = today.getHours();
+
+		if (currentHour >= 6 && currentHour < 18) {
+			setCurrentTheme(lightTheme);
+		} else {
+			setCurrentTheme(darkTheme);
+		}
+	}, []);
+
+	return (
+		<ThemeProvider theme={currentTheme}>
+			<GlobalStyle />
+		</ThemeProvider>
+	);
 }
---
+++ themes.js
@@ -0,0 +1,7 @@
+export const lightTheme = {
+	primary: '#FDFDFD',
+};
+
+export const darkTheme = {
+	primary: '#1D1D1D',
+};

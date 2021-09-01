cc4b9d22248e026a6c9c7ddf70f2aa03c46a6691
Hudson Lessa <admin@hudsonlessa.com>
2021-09-01 15:40:30 -0300

    Add custom fonts

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+@font-face {
+  font-family: "Bebas Neue";
+  src: url('../assets/fonts/BebasNeue-Regular.ttf');
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: "Raleway";
+  src: url('../assets/fonts/Raleway-Regular.ttf');
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: "Raleway";
+  src: url('../assets/fonts/Raleway-Bold.ttf');
+  font-weight: 700;
+}
+
 *, *::before, *::after {
   box-sizing: border-box;
   padding: 0;

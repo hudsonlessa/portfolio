e21896f57988cfd7d526469f88832cd51e751958
Hudson Lessa <admin@hudsonlessa.com>
2021-09-02 10:58:24 -0300

    Add hero background videos path to themes file

--- src/themes.js
+++ src/themes.js
@@ -1,7 +1,9 @@
 export const lightTheme = {
+	videoPath: "./src/assets/videos/valley_day.mp4",
 	primary: '#FDFDFD',
 };
 
 export const darkTheme = {
+	videoPath: "./src/assets/videos/valley_night.mp4",
 	primary: '#1D1D1D',
 };

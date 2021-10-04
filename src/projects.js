6c50032be10fab80b3bad2666f145ef8eb8480f9
Hudson Lessa <admin@hudsonlessa.com>
2021-10-04 20:00:15 -0300

    Add projects data file

---
+++ src/projects.js
@@ -0,0 +1,7 @@
+import superGenericPlatformGame from './assets/images/super-generic-platform-game.gif';
+
+export const projects = [{
+  title: "Super Generic Platform Game",
+  image: superGenericPlatformGame,
+  url: "http://hudsonlessa.itch.io/super-generic-platform-game"
+}];

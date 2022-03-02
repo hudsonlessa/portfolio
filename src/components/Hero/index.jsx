f07cd24c99c12d7eabb0a406744e315cabd127ef
Hudson Lessa <admin@hudsonlessa.com>
2022-03-02 13:52:16 -0300

    Pass value as string to video poster attribute on Hero component

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,7 +5,7 @@ import { Container, Subtitle, Title, Video } from './styles';
 export default function Hero({ texts, theme }) {
   return (
     <Container>
-      <Video src={theme.video} poster autoPlay muted loop playsInline />
+      <Video src={theme.video} poster="true" autoPlay muted loop playsInline />
       <Title>{texts.title}</Title>
       <Subtitle>{texts.subtitle}</Subtitle>
     </Container>

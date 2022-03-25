a47efa90594862a34e2dd0cafa04d3a912b98067
Hudson Lessa <admin@hudsonlessa.com>
2022-03-25 08:50:10 -0300

    Import LTC Bodoni 175 from Adobe Fonts

--- src/styles/global.js
+++ src/styles/global.js
@@ -13,6 +13,13 @@ export default createGlobalStyle`
 }
 
 @font-face {
+  font-family: "LTC Bodoni 175";
+  src: url("https://use.typekit.net/af/04715e/000000000000000077359a54/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2"),
+    url("https://use.typekit.net/af/04715e/000000000000000077359a54/30/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff"),
+    url("https://use.typekit.net/af/04715e/000000000000000077359a54/30/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("opentype");
+}
+
+@font-face {
   font-family: "Bebas Neue";
   src: url(${BebasNeueRegular});
 }

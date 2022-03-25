1868767b6f6c7a7c6113d5505fc76108c2d8de77
Hudson Lessa <admin@hudsonlessa.com>
2022-03-25 12:55:43 -0300

    Fix InfiniteLights being pushed down

--- src/components/InfiniteLights/styles.js
+++ src/components/InfiniteLights/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
   height: 100vh;
   overflow: hidden;
 `;

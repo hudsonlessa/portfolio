375383c3122cd32a4e23c1d7741b372e3efac4b9
Hudson Lessa <admin@hudsonlessa.com>
2022-03-25 11:39:11 -0300

    Display Header on mobile, whilst hiding Hint

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,24 +1,29 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-  display: none;
+  pointer-events: none;
+  display: flex;
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  padding: 20px;
+  font-size: 16px;
 
   @media (min-width: 768px) {
-    pointer-events: none;
-    display: flex;
-    position: absolute;
-    z-index: 1;
-    width: 100%;
     padding: 40px 80px;
-    font-size: 16px;
   }
 `;
 
 export const Hint = styled.p`
-  user-select: none;
-  margin-left: auto;
-  font-family: 'Raleway';
-  color: #c819ff;
+  display: none;
+
+  @media (min-width: 768px) {
+    display: initial;
+    user-select: none;
+    margin-left: auto;
+    font-family: 'Raleway';
+    color: #c819ff;
+  }
 `;
 
 export const LanguageSelector = styled.ul`

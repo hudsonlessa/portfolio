5f44d11ea12b02de6c91ce2bb79e1943406b73dc
Hudson Lessa <admin@hudsonlessa.com>
2022-03-25 11:31:36 -0300

    Add LanguageSelector to Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import { Container, Hint } from './styles';
+import { Container, Hint, LanguageSelector } from './styles';
 
-export default function Header({texts}) {
+export default function Header({ texts }) {
   return (
     <Container>
+      <LanguageSelector>
+        <li>US</li>
+        <li>BR</li>
+      </LanguageSelector>
       <Hint>{texts.hint}</Hint>
     </Container>
   );

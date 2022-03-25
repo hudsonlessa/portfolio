56bfc218391197dd252a800d125f72615429112d
Hudson Lessa <admin@hudsonlessa.com>
2022-03-25 09:50:27 -0300

    Remove videos reference from themes data

--- src/themes.js
+++ src/themes.js
@@ -1,14 +1,9 @@
-import valleyDay from './assets/videos/valley_day.mp4';
-import valleyNight from './assets/videos/valley_night.mp4';
-
 export const lightTheme = {
-  video: valleyDay,
   background: { primary: '#fdfdfd', secondary: '#f5f5f5' },
   text: { primary: '#1d1d1d', secondary: '#5d5d5d' },
 };
 
 export const darkTheme = {
-  video: valleyNight,
   background: { primary: '#1d1d1d', secondary: '#151515' },
   text: { primary: '#fdfdfd', secondary: '#5d5d5d' },
 };
